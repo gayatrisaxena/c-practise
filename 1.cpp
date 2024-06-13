1a0e0c689d2fe551b99f8ef92bc52accd06006b7
gayatrisaxena <143270782+gayatrisaxena@users.noreply.github.com>
2024-06-13 14:37:31 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,150 @@
+ #include<iostream>
+using namespace std;
+int main(){
+
+	int apples;
+	cin>>apples;
+	cout<<"apples:"<<apples<<endl;
+	return 0;
+}
+
+/*
+int main(){
+	int a=8;
+	cout<<a;
+	return 0;
+} */
+ 
+ // swap string
+/* # include<iostream>
+ using namespace std;
+ int main(){
+ 	int a, b;
+ 	a=5;
+ 	b=6;
+ 	cout<<a<<endl;
+ 	cout<<b<<endl;
+ 	int c;
+ 	c =b;
+ 	
+ 	b=a;
+ 	a=c;
+ 	cout<<a<< endl;
+ 	cout<<b<<endl;
+ 	
+ 	
+ 	return 0;
+ }*/
+ 
+ /*Arthemetic operators
+ #include<iostream>
+ using namespace std;
+  int main(){
+  	 int a=6;
+  	 int b=8;
+  	 cout<<a+b<<endl;
+  	 cout<<a-b<<endl;
+  	 cout<<a%b<<endl;
+  	
+  	return 0;
+  } */
+  
+  //relational operators
+  #include<iostream>
+  using namespace std;
+  int main(){
+  	/*
+  	int a=6;
+  	 int b=8;
+  	 cout<<(a==b)<<endl;
+  	 cout<<(a!=b)<<endl;
+  	 cout<<(a>=b)<<endl;
+  	 // assignment operators
+  	 cout<<(a+=1)<<endl;
+  	 cout<<(b-=1)<<endl;
+  	 // bitwise operators
+  	 cout<<(a|b)<<endl;
+  	 cout<<(a or b)<<endl;
+  	 // misc operators
+  	 int c=3;
+  	 cout<<sizeof(c)<<endl;
+  	 cout<<(++c)<<endl;
+  	cout<<(c--)<<endl;
+  	//if else condition : print even or add
+  	int num;
+  	cin>>num;
+  	if(num%2){
+  		cout<<"even"<<endl;
+  	    
+	  } else{
+	  	cout<<"odd"<<endl;
+	  }
+	  // prime no or not
+	  int no;
+	  cin>>no;
+	  if(no%2){
+	  	cout<<"isaprimeno"<<endl;
+	    }else{
+	  	cout<<"primenonot"<<endl;
+	  }
+	  
+	  //if else if else codition 
+	  int marks;
+	  cin>>marks;
+	  if(marks>80){
+	  	cout<<"A"<<endl;
+	  } else if(marks>60){
+	  	cout<<"B"<<endl;
+	  } else if(marks<60){
+	  	cout<<"C"<<endl;
+	  } else{
+	  	cout<<"F"<<endl;
+	  }
+	  //nested if else condition
+	if(marks>>33){
+		if(marks>>80){
+			cout<<"A"<<endl;
+		}else {
+			cout<<"pass"<<endl;
+		}
+	}else{
+		cout<<"F"<<endl;
+	}
+	//switch case
+	int day;
+	cin>>day;
+	switch(day){
+		case 1:
+			cout<<"Monday"<<endl;
+			break;
+	
+	    case 2:
+	         cout<<"tuesday" <<endl;
+	         break;
+	         
+	    case 3:
+		    cout<<"wednesday"<<endl;  
+			break; 
+		}
+	//loop
+	int i=0;
+	while(i<10){
+		cout<<i<<endl;
+		i++;
+	}
+	for(int i=0; i<3; i++)	
+	 cout<<i<<endl; */
+  
+  //do while loop
+     int n;
+    cin>>n; 
+    int sum=0;
+    do{
+  	int num;
+  	cin>>num;
+  	sum+=num;
+  	n--;
+    } while(n>0)
+  	cout<<sum<<endl;
+  	return 0;
+    } 
---
+++ function.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+/*
+int add(int num1, int num2){
+	int sum=num1+num2;
+	return sum;
+
+}
+int add(int num1, int num2, int num3){
+	int sum=num1+num2+ num3;
+	return sum;
+}
+ int main(){
+ 	 int a=4;
+ 	 int b=7;
+ 	 float c=2.3;
+ 	cout<<add(a,b,c)<<endl;
+ 	// standard library function
+ 	cout<<pow(2,3)<<endl;
+ 	cout<<sqrt(16)<<endl;    // standard library using cmath header file
+ 	int ans= sqrt(25);
+ 	cout<<ans<<endl; */
+ 	
+ 	// function
+	/*int sub(int num1 , int num2){
+ 	int sub=num1-num2;
+ 	return sub;
+    }
+     int main() {
+    	int a=9;
+    	int b=6;
+    	cout<<sub(a,b)<<endl;
+    	
+    //define global variable and local variable  */
+	/*string name="pari";
+	void fun(){
+		cout<<name<<"\n";
+     }
+    int main(){ 
+	
+    cout<<name<<"\n"; 
+	name="gayatri" ;  
+	fun(); */
+	/*string name="gayatri";
+	int main(){
+		cout<<name<<"\n";
+    
+ 	return 0;
+ } */
+    
+// exercise
+int main(){
+	/*
+int a=89;
+int b=90;
+int sum=a+b;
+
+cout<<sum<<endl;
+cout<<(a/b)<<endl;
+cout<<sqrt(16)<<endl;  
+*/
+
+    bool checkEligible(int age, int limit){
+	if(age>limt){
+		return true;
+	 }else{
+		return false;
+     }
+     }
+    int main(){
+    	int age;
+    	cin>>age;
+    	int limit;
+    	cin>>limit;
+    	cout<<age<<endl;
+    	cout<<limit<<endl;
+    	cout<<(age>limit)<<true<<endl;
+    	return 0;
+      
+    }
+        
+ 
